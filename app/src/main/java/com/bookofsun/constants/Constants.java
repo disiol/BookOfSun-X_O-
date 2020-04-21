d6a050ea8530827a558944dc13832860c3cf4e91
denis <025896321Denis>
2020-04-21 13:10:02 +0300

    setPlayerTyrn()

--- app/src/main/java/com/bookofsun/constants/Constants.java
+++ app/src/main/java/com/bookofsun/constants/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 
     public static final String URL_FOR_CHECK = "aHR0cHM6Ly9rbGRjcGEuc2l0ZS8=";
     public static final String URI_STRING = "aHR0cHM6Ly9rcmVzdGlraWFkdi5mdW4vN25mRGdnCg==";
-    public static final String DEPLINK = "pls://run";
+    public static final String DEPLINK = "brncv://run";
 
 
 
